08ab809726141f54fcbc95b474fe1ee3685e725d
Julian Mullins <jmullins1@babson.edu>
2015-02-01 13:55:42 -0500

    drunkcalc

---
+++ drunkcalc.py
@@ -0,0 +1,28 @@
+import time
+import Tkinter
+
+top = Tkinter.Tk()
+
+
+initmess = Tkinter.Message (top, text="Welcome to the BAC calculator")
+
+top.mainloop()
+
+"""
+print "Welcome to the alcohol calculator"
+print " " 
+time.sleep(1)
+
+def getInfo():
+		weight = raw_input("Enter your weight in pounds: ")
+		#drink_type = raw_input("Enter the type of drink you've had: ")
+		drink_quant = raw_input("Enter the number of (standard size) drink amounts you've had: ")
+		print "Thank you for the information. Currently processing how drunk you are..."
+		#drunkCalculator()
+		time.sleep(3)
+		print " "
+		print "Since you weigh " + weight + " pounds, and you've had " + drink_quant + " drinks, your blood alcohol level is: "
+
+getInfo()
+
+"""
